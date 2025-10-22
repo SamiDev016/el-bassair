e3e8c74755204578964e2ad2d692ffa6e799f24b
SamiDev016 <adelim0555@gmail.com>
2025-10-22 14:07:06 +0100

    ss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-3xl p-10 bg-red-500 hover:bg-green-500 cursor-pointer">hello</h1>
-    <p class="text-3xl font-bold bg-red-500 p-4 hover:bg-yellow-500 cursor-pointer">Welcome</p>
+<div>
+    <h1 class="font-bold text-3xl p-10 flex flex-col justify-center h-full w-full items-center hover:text-green-500 transition duration-500 cursor-pointer
+    border border-gray-500">
+        Empty Page</h1>
+
+</div>
 @endsection
